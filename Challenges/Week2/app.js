eb6ab305bec38f8d7af3417ba7071469731bc017
jeanettie <jeanettienicole@gmail.com>
2016-09-08 16:01:58 -0400

    added if else to code, etc

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,15 @@
 //create function
   //control and return statements
+ var person = prompt("So? What do you say, today?");
+ //return person.toLowerCase();
 
 //Prompt user for input and store variable
+if (person.toLowerCase() === "hello"){
+	alert("Hello World!");
+} else {
 
+	alert("Incorrect input, thanks but no thanks");
+}
 //Call function
 
 //Alert user results
